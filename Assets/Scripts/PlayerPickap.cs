36b366dd920093f4c03893be3424bfe9104a756a
Pelmex1 <89773802+Pelmex1@users.noreply.github.com>
2023-09-19 19:48:22 +0300

    Revert "Sborka"
    This reverts commit 571faa0cf9e0ba7173d51128cc9e0de3fc7c61b0.

--- Assets/Scripts/PlayerPickap.cs
+++ Assets/Scripts/PlayerPickap.cs
@@ -1,28 +1,25 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerPickap : MonoBehaviour
 {
-    [SerializeField] private float minX;
-    [SerializeField] private float minZ;
-    [SerializeField] private float maxX;
-    [SerializeField] private float maxZ;
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "stick")
-        {
+    private void OnCollisionEnter(Collision collision) {
+        Debug.Log("test");
+        if(collision.gameObject.tag == "stick"){
             collision.gameObject.SetActive(false);
             StartCoroutine(enableStick(collision.gameObject));
         }
     }
-        private IEnumerator enableStick(GameObject targetObject){
+    private IEnumerator enableStick(GameObject targetObject){
+        Debug.Log("startCorotine");
         targetObject.transform.position = Teleportpositions(targetObject.transform.position);
         targetObject.SetActive(true);
-        yield return new WaitForSecondsRealtime(10);
+        yield return new WaitForSeconds(100);
     }
     private Vector3 Teleportpositions(Vector3 positionNow){
         float randomX,randomZ;
-        randomX = Random.Range(minX,maxX); randomZ = Random.Range(minZ,maxZ);
+        randomX = Random.Range(-20,28.99f); randomZ = Random.Range(-24,25);
         Vector3 newPosition = new Vector3(positionNow.x + randomX,positionNow.y,positionNow.z + randomZ);
         return newPosition;
     }
